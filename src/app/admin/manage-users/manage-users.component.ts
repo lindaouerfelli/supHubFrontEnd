ef601da02f39a17fc6ec8cbe9ea6e57c049c9d66
Linda Ouerfelli <lindaouerfeli@gmail.com>
2024-02-29 13:07:11 +0100

    vF1 front

--- src/app/admin/manage-users/manage-users.component.ts
+++ src/app/admin/manage-users/manage-users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserDto} from "../../services/models/user-dto";
+import {UsersService} from "../../services/services/users.service";
 
 @Component({
   selector: 'app-manage-users',
@@ -7,9 +9,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageUsersComponent implements OnInit {
 
-  constructor() { }
+  customers: Array<UserDto> = [];
+  showInactiveUserOnly = false;
+  userIdToUpdate = -1;
+  updateState: boolean | undefined;
+
+  constructor(
+    private userService: UsersService
+
+  ) { }
 
   ngOnInit(): void {
+
+    this.findAllCustomers();
   }
+
+  private findAllCustomers() {
+    this.userService.findAll()
+      .subscribe({
+        next: (value) => {
+          this.customers = value;
+        }
+      });
+  }
+  filterCustomers() {
+    if (this.showInactiveUserOnly) {
+      this.customers = this.customers.filter((c) => !c.active);
+    } else {
+      this.findAllCustomers();
+    }
+  }
+
+
+  changeUserState(active: boolean | undefined, id: number | undefined) {
+    this.userIdToUpdate = id as number;
+    this.updateState = active;
+  }
+
+  updateUserState() {
+    if (this.updateState) {
+      this.userService.validateAccount({
+        'user-id': this.userIdToUpdate as number
+      }).subscribe({
+        next: () =>{
+          this.findAllCustomers();
+        }
+      });
+    } else {
+      this.userService.invalidateAccount({
+        'user-id': this.userIdToUpdate as number
+      }).subscribe({
+        next: () =>{}
+      });
+    }
+  }
+
+  cancelUpdate() {
+    const user = this.customers.find((c) =>c.id === this.userIdToUpdate);
+    if (user) {
+      user.active = !user.active
+    }
+    this.userIdToUpdate = -1;
+    this.updateState = undefined
+  }
+
+
 
 }
